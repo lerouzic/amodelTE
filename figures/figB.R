86dd8da67a2311b1630c55e1a6a9c50ea8ed9588
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2020-10-13 18:02:34 +0200

    New figures to explore different variants of the model + cosmetic improvements. FigB now uses the theoretical results.

--- figures/figB.R
+++ figures/figB.R
@@ -7,15 +7,36 @@ pdf("figB.pdf", width=8, height=12)
 u.seq <- 10^seq(-3,0,length.out=21)
 pi.seq <- seq(0.01,0.2,length.out=21)
 n0.seq <- seq(0.01, 2, length.out=11)
-layout(rbind(1:2,3:4,5:6))
-par(cex=1, mar=c(4,5,1,0))
-plot(u.seq, sapply(u.seq, function(u) mod1.timetoeq(u, rho=0.03, thresh=0.90)$t ), xlab="u", ylab="Time to 90% regulation", type="l", log="xy")
-plot(u.seq, sapply(u.seq, function(u) mod1.timetoeq(u, rho=0.03, thresh=0.90)$n ), xlab="u", ylab="Copy number at equilibrium", type="l", log="x", ylim=c(0, 35))
+k.seq <- 1:10
 
-plot(pi.seq, sapply(pi.seq, function(pi) mod1.timetoeq(u=0.2, rho=pi, thresh=0.90)$t ), xlab=expression(pi), ylab="Time to 90% regulation", type="l", ylim=c(0,80))
-plot(pi.seq, sapply(pi.seq, function(pi) mod1.timetoeq(u=0.2, rho=pi, thresh=0.90)$n ), xlab=expression(pi), ylab="Copy number at equilibrium", type="l")
+default.par <- list(u=0.1, pi=0.03, s=0, k=1, n0=1, selk=FALSE, p0=0)
 
-plot(n0.seq, sapply(n0.seq, function(n0) mod1.timetoeq(u=0.2, rho=0.03, n0=n0, thresh=0.90)$t ), xlab=expression(n[0]), ylab="Time to 90% regulation", type="l", ylim=c(0,100))
-plot(n0.seq, sapply(n0.seq, function(n0) mod1.timetoeq(u=0.2, rho=0.03, n0=n0, thresh=0.90)$n ), xlab=expression(n[0]), ylab="Copy number at equilibrium", type="l", ylim=c(0, 35))
+get.eq <- function(default.par, focal.par, what="Eq") {
+	pp <- default.par
+	pp[names(focal.par)] <- focal.par
+	do.call(pred.eq, pp)[[what]]$n
+}
+
+get.tmax <- function(default.par, focal.par, Tmax=1000, prox=0.9) {
+	pp <- default.par
+	pp[names(focal.par)] <- focal.par
+	dd <- do.call(amodel, c(pp, list(Tmax=Tmax)))$n
+	which(dd >= prox*max(dd))[1]
+}
+
+layout(rbind(1:2,3:4,5:6,7:8))
+
+par(cex=1, mar=c(4,5,1,1))
+plot(u.seq, sapply(u.seq, function(u) get.tmax(default.par, c(u=u)) ), xlab="u", ylab="Time to 90% max copies", type="l", log="xy")
+plot(u.seq, sapply(u.seq, function(u) get.eq(default.par, c(u=u)) ), xlab="u", ylab="Copy number at equilibrium", type="l", log="x", ylim=c(0, 35))
+
+plot(pi.seq, sapply(pi.seq, function(pi) get.tmax(default.par, c(pi=pi)) ), xlab=expression(pi), ylab="Time to 90% max copies", type="l")
+plot(pi.seq, sapply(pi.seq, function(pi) get.eq(default.par, c(pi=pi)) ), xlab=expression(pi), ylab="Copy number at equilibrium", type="l")
+
+plot(n0.seq, sapply(n0.seq, function(n0) get.tmax(default.par, c(n0=n0)) ), xlab=expression(n[0]), ylab="Time to 90% max copies", type="l")
+plot(n0.seq, sapply(n0.seq, function(n0) get.eq(default.par, c(n0=n0)) ), xlab=expression(n[0]), ylab="Copy number at equilibrium", type="l", ylim=c(0, 35))
+
+plot(k.seq, sapply(k.seq, function(k) get.tmax(default.par, c(k=k)) ), xlab="k", ylab="Time to 90% max copies", type="l", log="y")
+plot(k.seq, sapply(k.seq, function(k) get.eq(default.par, c(k=k)) ), xlab="k", ylab="Copy number at equilibrium", type="l", log="", ylim=c(0, 400))
 
 dev.off()
--- figures/figD.R
+++ figures/figD.R
@@ -4,7 +4,7 @@ source("../src/amodel.R")
 
 
 # neutral model
-pdf("figC.pdf", width=8, height=8)
+pdf("figD.pdf", width=8, height=8)
 
 Tmax <- 150
 
@@ -13,14 +13,14 @@ lty.eq <- 2
 
 col <- 1:10
 
-N.sim <- 10000
+N.sim <- 1000
 rep.sim <- 6
-Tmax <- 150
+Tmax <- 500
 
 use.cache=TRUE
 
 layout(rbind(1:2,3:4))
-model.default <- c(u=0.1, pi=0.03, s=0.01, k=1, selk=FALSE, n0=1, p0=0)
+model.default <- c(u=0.1, pi=0.03, s=0.01, k=1, selk=TRUE, n0=1, p0=0)
 
 model.par.s <- list(c(s=0), c(s=0.01), c(s=0.02))
 plot.model.dyn(model.default, model.par.s, what="n", pred=TRUE, sim=TRUE, legend=TRUE, legend.pos="bottomright", Tmax=Tmax, N=N.sim, rep=rep.sim, use.cache=use.cache)
