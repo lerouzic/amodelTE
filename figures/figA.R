a7f34798442305b209846c8aca53d53d89f7fb28
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2021-04-02 14:26:08 +0200

    Improving the figure quality.

--- figures/figA.R
+++ figures/figA.R
@@ -4,10 +4,6 @@ source("../src/amodel.R")
 
 
 # neutral model
-pdf("figA.pdf", width=8, height=8)
-
-Tmax <- 150
-
 lty.max<- 3
 lty.eq <- 2
 
@@ -15,23 +11,37 @@ col <- 1:10
 
 N.sim <- 10000
 rep.sim <- 1
-Tmax <- 150
+Tmax <- 300
 
 use.cache=TRUE
-
-layout(rbind(1:2, 3:4))
 model.default <- c(u=0.1, pi=0.03, s=0, k=1, sp=0, n0=1, p0=0)
 
+
+pdf("figA.pdf", width=8, height=6)
+layout(rbind(1:2, 3:4))
+par(mar=c(1,4,1,1), oma=c(3,0,0,0))
 
 ###### Effect of transposition rate
 model.par.u <- list(c(u=0.2), c(u=0.1), c(u=0.05))
 
-plot.model.dyn(model.default, model.par.u, what="n", pred=TRUE, sim=TRUE, legend=TRUE, legend.pos="bottomright", Tmax=Tmax, N=N.sim, rep=rep.sim, use.cache=use.cache)
-plot.model.dyn(model.default, model.par.u, what="p", pred=TRUE, sim=TRUE, legend=FALSE, Tmax=Tmax, N=N.sim, rep=rep.sim, use.cache=TRUE)
+plot.model.dyn(model.default, model.par.u, what="n", pred=TRUE, sim=TRUE, legend=TRUE, legend.pos="bottomright", Tmax=Tmax, N=N.sim, rep=rep.sim, use.cache=use.cache, xlab="", xaxt="n")
+axis(1, labels=FALSE)
+text(10, 36, expression(hat(n)))
 
-model.par.pi <- list(c(pi=0.01), c(pi=0.03), c(pi=0.1))
+plot.model.dyn(model.default, model.par.u, what="p", pred=TRUE, sim=TRUE, legend=FALSE, Tmax=Tmax, N=N.sim, rep=rep.sim, use.cache=TRUE, xlab="", xaxt="n")
+axis(1, labels=FALSE)
+text(10, 1.05, expression(hat(p)))
 
-plot.model.dyn(model.default, model.par.pi, what="n", pred=TRUE, sim=TRUE, legend=FALSE, Tmax=Tmax, N=N.sim, rep=rep.sim, use.cache=use.cache)
-plot.model.dyn(model.default, model.par.pi, what="p", pred=TRUE, sim=TRUE, legend=TRUE, legend.pos="bottomright", Tmax=Tmax, N=N.sim, rep=rep.sim, use.cache=TRUE)
+
+model.par.k <- list(c(k=1), c(k=2), c(k=3))
+
+plot.model.dyn(model.default, model.par.k, what="n", pred=TRUE, sim=TRUE, legend=FALSE, Tmax=Tmax, N=N.sim, rep=rep.sim, use.cache=use.cache, xlab="", xaxt="n")
+par(xpd=NA); axis(1); mtext("Time (generations)", side=1, line=2.5, cex=0.8); par(xpd=FALSE)
+text(10, 95, expression(hat(n)))
+
+plot.model.dyn(model.default, model.par.k, what="p", pred=TRUE, sim=TRUE, legend=TRUE, legend.pos="bottomright", Tmax=Tmax, N=N.sim, rep=rep.sim, use.cache=TRUE)
+par(xpd=NA); axis(1); mtext("Time (generations)", side=1, line=2.5, cex=0.8); par(xpd=FALSE)
+text(10, 1.05, expression(hat(p)))
+
 
 dev.off()
--- figures/figD.R
+++ figures/figD.R
@@ -2,33 +2,40 @@
 
 source("../src/amodel.R")
 
-
-# neutral model
+# deleterious TEs - deleterious clusters
 pdf("figD.pdf", width=8, height=8)
-
-Tmax <- 150
 
 lty.max<- 3
 lty.eq <- 2
 
 col <- 1:10
 
-N.sim <- 5000
-rep.sim <- 6
-Tmax <- 500
+N.sim <- 10000
+rep.sim <- 1
+Tmax <- 300
 
 use.cache=TRUE
+pdf("figD.pdf", width=8, height=6)
 
-layout(rbind(1:2,3:4))
+layout(rbind(1:2, 3:4))
+par(mar=c(1,4,1,1), oma=c(3,0,0,0))
+
 model.default <- c(u=0.1, pi=0.03, s=0.01, k=1, sp=0.01, n0=1, p0=0)
 
-model.par.s <- list(c(s=0), c(s=0.01), c(s=0.02))
-plot.model.dyn(model.default, model.par.s, what="n", pred=TRUE, sim=TRUE, legend=TRUE, legend.pos="bottomright", Tmax=Tmax, N=N.sim, rep=rep.sim, use.cache=use.cache)
-plot.model.dyn(model.default, model.par.s, what="p", pred=TRUE, sim=TRUE, legend=FALSE, Tmax=Tmax, N=N.sim, rep=rep.sim, use.cache=TRUE)
+model.par.s <- list(c(s=0, sp=0), c(s=0.01, sp=0.01), c(s=0.02, sp=0.02))
 
+plot.model.dyn(model.default, model.par.s, what="n", pred=TRUE, sim=TRUE, legend=FALSE, Tmax=Tmax, N=N.sim, rep=rep.sim, use.cache=use.cache, xlab="", xaxt="n")
+axis(1, labels=FALSE)
+axis(1, labels=FALSE)
+
+plot.model.dyn(model.default, model.par.s, what="p", pred=TRUE, sim=TRUE, legend=FALSE, Tmax=Tmax, N=N.sim, rep=rep.sim, use.cache=TRUE, xlab="", xaxt="n")
+axis(1, labels=FALSE)
+legend("bottomright", paste0("s=", sapply(model.par.s, "[", "s")), lty=1, col=seq_along(model.par.s), bty="n")
 
 model.par.k <- list(c(k=1), c(k=2), c(k=5))
-plot.model.dyn(model.default, model.par.k, what="n", pred=TRUE, sim=TRUE, legend=TRUE, legend.pos="bottomright", Tmax=Tmax, N=N.sim, rep=rep.sim, use.cache=use.cache)
-plot.model.dyn(model.default, model.par.k, what="p", pred=TRUE, sim=TRUE, legend=FALSE, Tmax=Tmax, N=N.sim, rep=rep.sim, use.cache=TRUE)
+plot.model.dyn(model.default, model.par.k, what="n", pred=TRUE, sim=TRUE, legend=FALSE, Tmax=Tmax, N=N.sim, rep=rep.sim, use.cache=use.cache, xlab="", xaxt="n")
+par(xpd=NA); axis(1); mtext("Time (generations)", side=1, line=2.5, cex=0.8); par(xpd=FALSE)
 
+plot.model.dyn(model.default, model.par.k, what="p", pred=TRUE, sim=TRUE, legend=TRUE, legend.pos="topleft", Tmax=Tmax, N=N.sim, rep=rep.sim, use.cache=TRUE)
+par(xpd=NA); axis(1); mtext("Time (generations)", side=1, line=2.5, cex=0.8); par(xpd=FALSE)
 dev.off()
